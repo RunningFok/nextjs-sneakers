3bc8075bcfe2a7fd6f9ecdb754f268f53952003a
runningfok <adrianfok26@gmail.com>
2023-03-02 00:25:03 +0800

    Added Dropbox in layout to various brands

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
-import './globals.css'
-import Link from 'next/link'
+"use client";
+import "./globals.css";
+import Link from "next/link";
+import { Dropdown } from "@nextui-org/react";
 
 export default function RootLayout({ children }) {
   return (
@@ -10,11 +12,31 @@ export default function RootLayout({ children }) {
       */}
       <head />
       <body>
-      <nav>
-      <Link href="/">Home</Link>
-      <Link href="/allshoes">All Shoes</Link>
-      </nav>
-      {children}</body>
+        <nav>
+          <Link href="/">Home</Link>
+          <Dropdown>
+            <Dropdown.Button flat>Brands</Dropdown.Button>
+            <Dropdown.Menu aria-label="Static Actions">
+              <Dropdown.Item key="airjordan">
+                <Link href="/brands/airjordan">Air Jordan</Link>
+              </Dropdown.Item>
+              <Dropdown.Item key="adidas">
+                <Link href="/brands/adidas">Adidas</Link>
+              </Dropdown.Item>
+              <Dropdown.Item key="hoka" href="/brands/hoka">
+                <Link href="/brands/hoka">Hoka</Link>
+              </Dropdown.Item>
+              <Dropdown.Item key="newbalance" href="/brands/newbalance">
+                <Link href="/brands/newbalance">New Balance</Link>
+              </Dropdown.Item>
+              <Dropdown.Item key="nike" href="/brands/nike">
+                <Link href="/brands/nike">Nike</Link>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </nav>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
